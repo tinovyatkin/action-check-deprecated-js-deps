6409c2b6f5c0228a35d4e9038667a2a6b254b6fc
Konstantin Vyatkin <tino@vtkn.io>
2019-12-12 20:22:59 -0400

    add ignore support

--- check-deprecations.js
+++ check-deprecations.js
@@ -1,11 +1,18 @@
 import npmQuery from "package-json";
 import * as semver from "semver";
+import * as core from "@actions/core";
 
 /**
  *
  * @param {Map<string, {file: string, type: 'dependencies' | 'devDependencies' | 'resolutions', version: string}[]>} allDeps
  */
 export default async function checkDeprecations(allDeps) {
+  const ignore =
+    core.getInput("ignore", {
+      required: false
+    }) || "";
+  const modulesToIgnore = new Set(ignore.split(/\s*,\s*/g).filter(v => v));
+
   /** @type {Set<string>} */
   const deprecated = new Set();
 
@@ -13,7 +20,7 @@ export default async function checkDeprecations(allDeps) {
     const tag = pkgs.find(({ version }) => !semver.valid(version));
     const latestUsedVersion = tag
       ? tag.version
-      : pkgs.sort((p1, p2) => (semver.gte(p1.version, p2.version) ? 1 : -1))[0]
+      : pkgs.sort((p1, p2) => (semver.gte(p1.version, p2.version) ? -1 : 1))[0]
           .version;
 
     const pi = await npmQuery(name, {
@@ -24,7 +31,8 @@ export default async function checkDeprecations(allDeps) {
       for (const pk of pkgs) {
         console.log("\t %s [%s]: %s", pk.file, pk.type, pk.version);
       }
-      deprecated.add(name);
+      if (modulesToIgnore.has(name)) console.warn("Ignoring...");
+      else deprecated.add(name);
     }
   }
   return deprecated;
