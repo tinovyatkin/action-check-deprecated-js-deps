9ecbb137c3b74c29d6ea5ce5180d84bdc3e71c83
Konstantin Vyatkin <tino@vtkn.io>
2019-12-12 21:14:11 -0400

    try/catch npm query

--- check-deprecations.js
+++ check-deprecations.js
@@ -23,16 +23,20 @@ export default async function checkDeprecations(allDeps) {
       : pkgs.sort((p1, p2) => (semver.gte(p1.version, p2.version) ? -1 : 1))[0]
           .version;
 
-    const pi = await npmQuery(name, {
-      version: latestUsedVersion
-    });
-    if (pi.deprecated) {
-      console.warn("📦  \x1b[1m\x1b[31m%s\x1b[0m: %s", name, pi.deprecated);
-      for (const pk of pkgs) {
-        console.log("\t %s [%s]: %s", pk.file, pk.type, pk.version);
+    try {
+      const pi = await npmQuery(name, {
+        version: latestUsedVersion
+      });
+      if (pi.deprecated) {
+        console.warn("📦  \x1b[1m\x1b[31m%s\x1b[0m: %s", name, pi.deprecated);
+        for (const pk of pkgs) {
+          console.log("\t %s [%s]: %s", pk.file, pk.type, pk.version);
+        }
+        if (modulesToIgnore.has(name)) console.warn("Ignoring...");
+        else deprecated.add(name);
       }
-      if (modulesToIgnore.has(name)) console.warn("Ignoring...");
-      else deprecated.add(name);
+    } catch (err) {
+      console.error(err);
     }
   }
   return deprecated;
